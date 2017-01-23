a9eb1ac6386fba7814c126d231a8b7f7c36dfee7
Vincent van de Wouw <Vincent van de Wouw>
2017-01-23 16:50:17 +0100

    push 4

--- reset_model.py
+++ reset_model.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys, os
-from openerp import models, fields
+from odoo import models, fields
+
+
 class ResetButton(models.Model):
     _name = 'reset.button'
+    
     def reset_database(self):
         try:
             os.system('su - postgres  -c "sh resetDemo.sh"')
