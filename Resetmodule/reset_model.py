ff792a5d37ae0285e5571dc64792207fc44e8670
Vincent van de Wouw <Vincent van de Wouw>
2017-01-23 14:48:04 +0100

    Update 2

--- Resetmodule/reset_model.py
+++ Resetmodule/reset_model.py
@@ -5,7 +5,7 @@ class ResetButton(models.Model):
     _name = 'reset.button'
     def reset_database(self):
         try:
-            os.system('su - root@demov10vincent@aeQu2Oquie3neBah:  -c "sh resetDemo.sh"')
+            os.system('su - postgres@demov10vincent:  -c "sh resetDemo.sh"')
         except:
             raise Exception("Reset function failed")
 
